994ef6fb1c5266066e5eea13ee6ed51bd1174f32
mohammedtakbir <takbirhossain678@gmail.com>
2023-02-09 14:36:17 +0600

    explore shortahand

---
+++ shorthand.js
@@ -0,0 +1,14 @@
+//* shorthand and variable declaration/update
+var mango = 45;
+mango = 34;
+mango += 66
+mango = mango + 100
+mango -= 100
+mango *= 5
+mango /= 10
+
+//* Increment/Decrement
+mango++;
+mango--;
+
+console.log(mango)
