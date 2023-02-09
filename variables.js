dee6d85e9b8fced09dd76035d270f157240400e1
mohammedtakbir <takbirhossain678@gmail.com>
2023-02-09 11:53:09 +0600

    variable types

--- variables.js
+++ variables.js
@@ -1,4 +1,12 @@
+//* Numeric/number datatypes
+var age = 22;
+var price = 33;
+
+//* string datatypes
 var name = 'Takbir';
 var age = '23';
 var profession = 'Developer';
+
+//* boolean datatypes
 var isMarried = false;
+var isSingle = true;
