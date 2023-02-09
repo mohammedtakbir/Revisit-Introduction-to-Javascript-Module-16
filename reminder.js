5bae6db21079baeba5c3c31ab8a573febf51261e
mohammedtakbir <takbirhossain678@gmail.com>
2023-02-09 15:32:54 +0600

    explore reminder and types

---
+++ reminder.js
@@ -0,0 +1,4 @@
+var num1 = 17;
+var num2 = 5;
+var reminder = num1 % num2;
+console.log(reminder)
---
+++ types.js
@@ -0,0 +1,5 @@
+var num1 = 0.1;
+var num2 = 0.2;
+var total = num1 + num2;
+total = total.toFixed(2);
+console.log(parseFloat(total))
