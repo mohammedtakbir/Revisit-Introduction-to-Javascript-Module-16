2ddec1bea860faa018203ea591905f197c604bbb
mohammedtakbir <takbirhossain678@gmail.com>
2023-02-09 15:52:47 +0600

    practice problem

---
+++ practiceProblem.js
@@ -0,0 +1,33 @@
+//* practice problem - 01
+// var taka = 800;
+// var applePrice = 400;
+// var orangePrice = 300;
+// var total = applePrice + orangePrice;
+// var remaining = taka - total;
+// console.log(remaining)
+
+
+//* practice problem - 02
+// var totalMarks = 75.25 + 65 + 80 + 35.45 + 99.50
+// var totalSub = 5;
+// var average = (totalMarks / totalSub).toFixed(2);
+// console.log(parseFloat(average))
+
+
+
+
+
+
+//* practice problem - 03
+// var firstLine = 'I am going to be';
+// var lastLine = 'an awesome web developer';
+// var fullLine = firstLine + ' ' + lastLine;
+// console.log(fullLine)
+
+
+//* practice problem - 04
+var givenNumber = 119;
+var reminder = givenNumber % 5;
+console.log(reminder)
+
+
