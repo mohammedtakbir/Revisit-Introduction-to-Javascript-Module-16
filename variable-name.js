bc00f35f5f41d57f2ea51fd6ad26ac1dc0452466
mohammedtakbir <takbirhossain678@gmail.com>
2023-02-09 13:48:17 +0600

    learn variable naming convention

---
+++ variable-name.js
@@ -0,0 +1,25 @@
+//* variable name cannot be any JS reserved keywords
+
+// var return = 'return'
+// var break = 'break'
+
+//* variable name has to be one word
+// var one man = 'army'
+// var user name = 'takbir';
+var firstName = 'mohammed';
+
+//* variable name cannot have quotation
+// var 'name' = 'name'
+
+//* variable name cannot use dash
+// var user-name = 'takbir'
+
+//* variable name cannot start with a number but ends with number
+// var 5ten = 'foawekop'
+// var six9 = 'afwefj' 
+
+
+//* variable naming convention for long name
+// var userCurrentAddress = 'afwe' //* camel case
+// var user_current_address = 'kfawefk' //* snake case
+// var UserName = 'faewaw' //* pascal case
