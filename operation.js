d0fdf8204e9ddfc8205e0f7f19e6509594e752cd
mohammedtakbir <takbirhossain678@gmail.com>
2023-02-09 14:16:03 +0600

    simple math operation

---
+++ operation.js
@@ -0,0 +1,21 @@
+var bananaPrice = 12;
+var applePrice = 24;
+
+//* addition
+var totalPrice = bananaPrice + applePrice;
+
+//* subtraction
+var priceDifference = applePrice - bananaPrice;
+
+//* multiplication
+var quantity = 5;
+var totalCost = bananaPrice * quantity;
+
+//* division
+var apple = 50;
+var people = 5;
+var perPeopleGet = apple / people;
+console.log(totalPrice)
+console.log(priceDifference)
+console.log(totalCost)
+console.log(perPeopleGet)
