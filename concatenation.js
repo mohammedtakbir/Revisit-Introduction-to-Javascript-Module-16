2d407fcf7aeb85dcd08d566d287102cd46f14764
mohammedtakbir <takbirhossain678@gmail.com>
2023-02-09 15:00:15 +0600

    explore string concat and conversion

---
+++ concatenation.js
@@ -0,0 +1,5 @@
+//* string concatenation
+var firstName = 'Mohammed';
+var lastName = 'Takbir';
+var fullName = firstName + ' ' + lastName;
+console.log(fullName)
---
+++ conversion.js
@@ -0,0 +1,5 @@
+//* type conversion
+var price1 = '40.55';
+var price2 = '60';
+var total = parseFloat(price1) + parseInt(price2);
+console.log(total)
